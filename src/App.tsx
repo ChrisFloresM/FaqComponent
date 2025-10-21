cccd7eb056552f260be932714b71062e3ec918c0
Christian <christian.mig.flmo@gmail.com>
2025-10-21 13:43:38 -0600

    Pushing completed setup

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div className="font-worksans">
+      <h1 className="text-preset-1 leading-preset-1 bg-purple-600 py-8 text-center text-2xl font-bold text-red-400 uppercase">
+        Testing tailwind - Preset 1
+      </h1>
+      <p className="text-preset-2 leading-preset-2 font-semibold">
+        Testing tailwind - Preset 2{" "}
+      </p>
+      <p className="text-preset-3 leading-preset-3 font-normal">
+        Testing tailwind - Preset 3{" "}
+      </p>
+    </div>
+  );
+}
+
+export default App;
